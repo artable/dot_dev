7257f11ba1bfaa0aab62960be70445d2538a3747
artable <r.jonah.deal@gmail.com>
2023-07-08 16:47:38 -0600

    another chrome fix

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,7 +1,6 @@
 const target = $("#target");
 
 function position(E) {
-    console.log(E)
     let start_pos = E.offset();
     return [start_pos.left, start_pos.top];
 }
@@ -16,15 +15,13 @@ function setDeltas(E, x, y, scale) {
 
 function computeScale(E) {
     // take up max 95% of the container
-    let maxHeight = (parseFloat(target.css("margin-top")) + 
-                 parseFloat(target.css("margin-bottom")) +
-                 target.height()) * .95;
-    let maxWidth = (parseFloat(target.css("margin-left")) + 
-                 parseFloat(target.css("margin-right")) +
-                 target.width()) * .95;
-
+    let maxHeight = target.parent().height() * .95;
+    let maxWidth = target.parent().width() * .95;
+    console.log(parseFloat(target.css("margin-left")));
+    console.log(`w/h: (${maxWidth}, ${maxHeight})`)
     let maxScaleX = maxWidth / E.width();
     let maxScaleY = maxHeight / E.height();
+    console.log(`maxes: (${maxScaleX}, ${maxScaleY})`)
     return Math.min(maxScaleX, maxScaleY);
 }
 function runAnimation(animated) {
@@ -32,12 +29,12 @@ function runAnimation(animated) {
     [width, height]  = getDimensions(animated);
     [targetX, targetY] = position(target);
     let scale = computeScale(animated);
-    console.log(scale);
     //height and width center the poster as apposed to targeting top left
     let deltaX = targetX - startX - width/2;
     let deltaY = targetY - startY - height/2;
     deltaX /= scale;
     deltaY /= scale;
+    console.log(`(${deltaX}, ${deltaY}, [${scale}])`)
     animated.addClass("animation");
     setDeltas(animated, deltaX, deltaY, scale);
     animated.css("display", "block");
