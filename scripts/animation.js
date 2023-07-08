92383a8e71065eec6b8f00d23d277eba3b8ee7a8
artable <r.jonah.deal@gmail.com>
2023-07-08 16:21:03 -0600

    variable scale for animation implemented

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,5 +1,4 @@
 const target = $("#target");
-const scale = 2;
 
 function position(E) {
     console.log(E)
@@ -9,21 +8,38 @@ function position(E) {
 function getDimensions(E) {
     return [E.width(), E.height()];
 }
-function setDeltas(E, x, y) {
+function setDeltas(E, x, y, scale) {
     E.css('--deltaX', `${x}px`);
     E.css('--deltaY', `${y}px`);
+    E.css('--scale',  `${scale}`);
+}
+
+function computeScale(E) {
+    // take up max 95% of the container
+    let maxHeight = (parseFloat(target.css("margin-top")) + 
+                 parseFloat(target.css("margin-bottom")) +
+                 target.height()) * .95;
+    let maxWidth = (parseFloat(target.css("margin-left")) + 
+                 parseFloat(target.css("margin-right")) +
+                 target.width()) * .95;
+
+    let maxScaleX = maxWidth / E.width();
+    let maxScaleY = maxHeight / E.height();
+    return Math.min(maxScaleX, maxScaleY);
 }
 function runAnimation(animated) {
     [startX, startY] = position(animated);
     [width, height]  = getDimensions(animated);
     [targetX, targetY] = position(target);
+    let scale = computeScale(animated);
+    console.log(scale);
     //height and width center the poster as apposed to targeting top left
     let deltaX = targetX - startX - width/2;
     let deltaY = targetY - startY - height/2;
     deltaX /= scale;
     deltaY /= scale;
     animated.addClass("animation");
-    setDeltas(animated, deltaX, deltaY);
+    setDeltas(animated, deltaX, deltaY, scale);
     animated.css("display", "block");
 }
 function restore(animated) {
